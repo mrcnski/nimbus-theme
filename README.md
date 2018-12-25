ccc4bdfec406501bfb94e76be5bbe9c80e68507e
Marcin S <scatman@bu.edu>
2018-12-25 20:01:09 +0100

    add Contributors section to README

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ The best dark theme for Emacs.
 
 ![Screenshot](screenshot.png)
 
+## About
+
+Nimbus is a dark theme that I developed for my personal use. Because it is the only theme I ever use, it is continuously being worked on. I regularly add and modify faces and am always looking to support more third-party packages.
+
+Nimbus was originally a fork of [Ample](https://github.com/jordonbiondo/ample-theme).
+
 ## Installing
 
 Make sure you have set up [MELPA](http://melpa.milkbox.net/#/getting-started) and run:
@@ -33,7 +39,15 @@ Or, if you have [use-package](https://github.com/jwiegley/use-package):
 If you encounter any issues with Nimbus (such as text faces being hard to read or not appearing as expected), please try the following:
 
 1. Restart Emacs and make sure Nimbus is the first theme you enable. Switching custom themes midway through an Emacs session may cause artifacts from the previous theme to remain.
-1. Leave a bug report for the repository on GitHub (https://github.com/m-cat/nimbus-theme/issues).
+1. Leave a bug report on [GitHub](https://github.com/m-cat/nimbus-theme/issues).
+
+## Contributors
+
+- Marcin Swieczkowski [[Github](https://github.com/m-cat)] -- The head honcho
+- Jordon Biondo [[Github](https://github.com/jordonbiondo)] -- Creator of [Ample Theme](https://github.com/jordonbiondo/ample-theme)
+- Davor Rotim [[Github](https://github.com/drot)]
+- Pierre Le Gall [[Github](https://github.com/lepieru)]
+- Bruno [[Github](https://github.com/arzoriac)]
 
 ## Contributing
 
--- nimbus-theme.el
+++ nimbus-theme.el
@@ -1,12 +1,13 @@
 ;;; nimbus-theme.el --- An awesome dark theme
 ;;
-;; Filename: nimbus-theme.el
+;; Filename:    nimbus-theme.el
 ;; Description: An awesome dark theme
-;; Author: Marcin Swieczkowski <scatman@bu.edu>
-;; Created: Thu Mar 2 22:19:19 CET 2017
-;; Version: 1.0.0
-;; URL: https://github.com/m-cat/nimbus-theme
-;; Keywords: faces
+;; Author:      Marcin Swieczkowski <marcin.swieczkowski@gmail.com>
+;;              See README.md for full list of contributors.
+;; Created:     Thu Mar 2 22:19:19 CET 2017
+;; Version:     1.0.0
+;; URL:         https://github.com/m-cat/nimbus-theme
+;; Keywords:    faces
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Commentary:
@@ -15,12 +16,7 @@
 ;;
 ;; The best dark theme for Emacs.
 ;;
-;; This theme was originally a fork of Ample theme, but has seen
-;; enough tweaks and changes that it's essentially a new (and better)
-;; theme.
-;;
-;; Nimbus theme is constantly being worked on, and I am actively
-;; adding faces and support for more third-party packages.
+;; See README.md for more info.
 ;;
 ;; Installing:
 ;;
@@ -46,9 +42,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
+
 (deftheme nimbus "An awesome dark theme.")
 
-;; Define the palette
+;; Define the palette.
 (let (
       (lightest-green "#8fbc8f")
       (light-green    "#9ccc65")
@@ -86,7 +83,7 @@
       (fg             "#bdbdb3")
       )
 
-  ;; Set faces
+  ;; Set faces.
   (custom-theme-set-faces
    `nimbus ;; You must use the same theme name here...
    `(default
